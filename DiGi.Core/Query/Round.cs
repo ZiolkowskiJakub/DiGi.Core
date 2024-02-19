9104c9a69897f4c4f6d36635e7b0f27c928c82f8
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-02-19 20:22:01 +0100

    Tolerance Constants Added

---
+++ DiGi.Core/Query/Round.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DiGi.Core
+{
+    public static partial class Query
+    {
+        public static double Round(this double value, double tolerance)
+        {
+            if (double.IsNaN(value) || double.IsInfinity(value))
+            {
+                return value;
+            }
+
+            if (tolerance.Equals(0.0))
+            {
+                return value;
+            }
+
+            return (double)(Math.Round((decimal)value / (decimal)tolerance) * (decimal)tolerance);
+        }
+    }
+
+}
