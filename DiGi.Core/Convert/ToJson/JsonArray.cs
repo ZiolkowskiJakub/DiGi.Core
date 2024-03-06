731e66552fe19c6cf47dd656e6d5a9cb1c9fc22a
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-03-06 22:06:28 +0100

    Convert to JsonArray Implemented

---
+++ DiGi.Core/Convert/ToJson/JsonArray.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Nodes;
+using DiGi.Core.Interfaces;
+
+namespace DiGi.Core
+{
+    public static partial class Convert
+    {
+        public static JsonArray ToJson<T>(this IEnumerable<T> serializableObjects) where T : ISerializableObject
+        {
+            if(serializableObjects == null)
+            {
+                return null;
+            }
+
+            JsonArray result = new JsonArray();
+            foreach(T serializableObject in serializableObjects)
+            {
+                JsonObject jsonObject = serializableObject?.ToJsonObject();
+                result.Add(jsonObject);
+            }
+
+            return result;
+        }
+    }
+}
