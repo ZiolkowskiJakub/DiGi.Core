0beeca10968c4d7e9124a235e66e649b6b813430
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-03-20 20:39:09 +0100

    Previous and Next Queries Implemented

---
+++ DiGi.Core/Query/Next.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DiGi.Core
+{
+    public static partial class Query
+    {
+        public static T Next<T>(this IEnumerable<T> values, int index)
+        {
+            if (values == null)
+            {
+                return default(T);
+            }
+
+            int index_Temp = Next(values.Count(), index);
+            if (index_Temp == -1)
+            {
+                return default;
+            }
+
+            return values.ElementAt(index_Temp);
+        }
+
+        public static int Next(this int count, int index)
+        {
+            if (index < 0)
+            {
+                return -1;
+            }
+
+            if (count == 0)
+            {
+                return -1;
+            }
+
+            int result = index + 1;
+            while (result >= count)
+            {
+                result -= count;
+            }
+
+            return result;
+        }
+    }
+
+}
---
+++ DiGi.Core/Query/Previous.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DiGi.Core
+{
+    public static partial class Query
+    {
+        public static T Previous<T>(this IEnumerable<T> values, int index)
+        {
+            if (values == null)
+            {
+                return default(T);
+            }
+
+            int index_Temp = Previous(values.Count(), index);
+            if (index_Temp == -1)
+            {
+                return default;
+            }
+
+            return values.ElementAt(index_Temp);
+        }
+
+        public static int Previous(this int count, int index)
+        {
+            if (index < 0)
+            {
+                return -1;
+            }
+
+            if (count == 0)
+            {
+                return -1;
+            }
+
+            int result = index;
+            while (result >= count)
+            {
+                result -= count;
+            }
+
+            result = result == 0 ? count - 1 : result - 1;
+
+            return result;
+        }
+    }
+
+}
