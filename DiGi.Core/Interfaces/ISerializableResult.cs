02bb1c806061d59087b8df23f359d8b292d762e9
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-05-23 09:14:33 +0200

    IResult and ISerializableResult Implemented

---
+++ DiGi.Core/Interfaces/ISerializableResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Nodes;
+
+namespace DiGi.Core.Interfaces
+{
+    public interface ISerializableResult : IResult, ISerializableObject
+    {
+        JsonObject ToJsonObject();
+
+        bool FromJsonObject(JsonObject jsonObject);
+
+        ISerializableObject Clone();
+    }
+}
