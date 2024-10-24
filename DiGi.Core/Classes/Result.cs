8e536ce1a9cb7db85457f42fd207069e9940bacc
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-10-24 21:09:55 +0200

    Result<T> Implemented

--- DiGi.Core/Classes/Result.cs
+++ DiGi.Core/Classes/Result.cs
@@ -23,4 +23,25 @@ namespace DiGi.Core.Classes
 
         }
     }
+
+    public abstract class Result<T> : Result, IResult<T> where T: ISerializableObject
+    {
+        public Result()
+            : base()
+        {
+
+        }
+
+        public Result(JsonObject jsonObject)
+            : base(jsonObject)
+        {
+
+        }
+
+        public Result(Result<T> result)
+            : base(result)
+        {
+
+        }
+    }
 }
