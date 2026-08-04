d47112694abdc76e2fc1a5ac299f65c634aef4eb
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2026-08-04 10:20:03 +0200

    Code cleaning

--- DiGi.Core/Classes/SphericalDistributionSerializableObjectCollection.cs
+++ DiGi.Core/Classes/SphericalDistributionSerializableObjectCollection.cs
@@ -50,13 +50,7 @@ namespace DiGi.Core.Classes
         /// <param name="minElevation">The minimum elevation value.</param>
         /// <param name="maxElevation">The maximum elevation value.</param>
         /// <param name="elevationInterval">The elevation bin interval size.</param>
-        public SphericalDistributionSerializableObjectCollection(
-            double minAzimuth,
-            double maxAzimuth,
-            double azimuthInterval,
-            double minElevation,
-            double maxElevation,
-            double elevationInterval)
+        public SphericalDistributionSerializableObjectCollection(double minAzimuth, double maxAzimuth, double azimuthInterval, double minElevation, double maxElevation, double elevationInterval)
             : this(new Range<double>(minAzimuth, maxAzimuth), azimuthInterval, new Range<double>(minElevation, maxElevation), elevationInterval)
         {
         }
